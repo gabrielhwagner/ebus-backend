649cf1805e18cdb55dae3fe82f89bc80ddbfb691
gabriel wagner <gabriel.wagner@ilegra.com>
2020-04-21 19:22:17 -0300

    Ajusta endpoint de salvar playerID

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -9,17 +9,33 @@ module.exports = {
     try {
       const { userId } = req.user;
       const { playerId } = req.body;
-      
+
       const user = await connection('indicador_notificacao as in')
         .select('*')
         .where('in.usuario_id', userId);
       
+      const equal = await connection('indicador_notificacao as in')
+        .select('*')
+        .where('in.app_id', playerId);
+      
       if (user.length === 1 && user[0].app_id !== playerId) {
+        if (equal.length > 0) {
+          await connection('indicador_notificacao as in')
+            .where('in.app_id', playerId)
+            .del();
+        }
+
         await connection('indicador_notificacao')
           .update({ app_id: playerId})
           .where({ usuario_id: userId });
 
       } else if (user.length === 0) {
+        if (equal.length > 0) {
+          await connection('indicador_notificacao as in')
+            .where('in.app_id', playerId)
+            .del();
+        }
+
         await connection('indicador_notificacao')
           .insert({
             app_id: playerId,
